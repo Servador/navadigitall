50ec8fa980701faa3563d0a3fb301a6b9a803f0a
Servador <faizrizqi1029@gmail.com>
2025-10-30 16:40:08 +0700

    deskripsi produk

--- server.js
+++ server.js
@@ -390,8 +390,12 @@ app.delete("/api/admin/variant/:id", verifyToken, (req, res) => {
 });
 
 app.put("/api/admin/variant/:id", verifyToken, (req, res) => {
-  const { title, price, stock } = req.body;
+  const { title, price, stock, description } = req.body;
   const variantId = req.params.id;
+
+  db.prepare(
+    "UPDATE product_variants SET title=?, price=?, stock=?, description=? WHERE id=?"
+  ).run(title, price, stock, description, variantId);
 
   // Update varian
   db.prepare(
